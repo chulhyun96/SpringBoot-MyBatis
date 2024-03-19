629087d6475cadc29f31976c3b97e1d22a020bd6
박철현 <tghy4@naver.com>
2024-03-19 09:42:08 +0900

    test. bean dependenct injection 테스트 코드 통과

--- src/main/java/com/example/Project1/controller/AdminController.java
+++ src/main/java/com/example/Project1/controller/AdminController.java
@@ -5,10 +5,10 @@ import com.example.Project1.entity.Product;
 import com.example.Project1.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,8 +17,8 @@ public class AdminController {
     private final ProductService productService;
 
     @PostMapping("/product")
-    public String addProduct(@ModelAttribute Product productToSave, Model model) {
-        model.addAttribute("product", productService.save(productToSave));
+    public String addProduct(@ModelAttribute Product productToSave, RedirectAttributes ra) {
+
         return "redirect:/index";
     }
 }
--- src/test/java/com/example/Project1/controller/AdminControllerTest.java
+++ src/test/java/com/example/Project1/controller/AdminControllerTest.java
@@ -4,17 +4,35 @@ import com.example.Project1.config.TestConfig;
 import com.example.Project1.entity.Product;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static org.mockito.Mockito.mock;
 
 class AdminControllerTest {
+
+    @Test
+    void dependencyInjection() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        Object bean = null;
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = context.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                bean = context.getBean(beanDefinitionName);
+                Assertions.assertThat(bean).isNotNull();
+                System.out.println("bean = "+ bean);
+            }
+        }
+    }
     @Test
     void addProduct() {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
         AdminController adminController = context.getBean(AdminController.class);
-        Model mockModel = mock(Model.class);
+        RedirectAttributes mockModel = mock(RedirectAttributes.class);
 
         // given
         Product product = new Product();
