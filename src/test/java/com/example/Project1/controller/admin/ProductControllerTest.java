ee91392bdcddac9cb10a99204f0483e5e5f0551a
박철현 <tghy4@naver.com>
2024-03-26 11:20:22 +0900

    detail 메서드 테스트 통과

--- src/test/java/com/example/Project1/controller/admin/ProductControllerTest.java
+++ src/test/java/com/example/Project1/controller/admin/ProductControllerTest.java
@@ -15,11 +15,11 @@ import static org.mockito.Mockito.mock;
 
 public class ProductControllerTest {
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+    ProductController controller = context.getBean(ProductController.class);
     @Test
     @DisplayName("상품 등록 테스트")
     void addProduct() {
         // given
-        ProductController productController = context.getBean(ProductController.class);
         RedirectAttributes mockRa = mock(RedirectAttributes.class);
 
         // when
@@ -42,7 +42,6 @@ public class ProductControllerTest {
     @DisplayName("리스트 출력 테스트")
     void listProducts() {
         // given
-        ProductController bean = context.getBean(ProductController.class);
         Model model = mock(Model.class);
 
         Product product = new Product();
@@ -54,10 +53,30 @@ public class ProductControllerTest {
 
         // when
         List<Product> list = List.of(product);
-        String viewPath = bean.list(model);
+        String viewPath = controller.list(model);
 
         // then
         Assertions.assertThat(list.size()).isEqualTo(1);
         Assertions.assertThat(viewPath).isEqualTo("admin/products/list");
+    }
+    @Test
+    @DisplayName("상품 상세페이지 테스트")
+    void getById() {
+        // given
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("test");
+        product.setSupplyPrice(30000);
+        product.setSellingPrice(30000);
+        product.setImg("testImgPath");
+        product.setDescription("testDescription");
+
+        // when
+        Model model = mock(Model.class);
+        String detailPath = controller.detail(product.getId(), model);
+
+        // then
+        Assertions.assertThat(product.getId()).isEqualTo(1);
+        Assertions.assertThat(detailPath).isEqualTo("admin/products/detail");
     }
 }
