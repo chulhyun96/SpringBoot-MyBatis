c1c3291ee91d9eb67b0cc00b9e66b700b18c6eae
박철현 <tghy4@naver.com>
2024-04-17 11:11:24 +0900

    deliveryType, category 메서드 수정

--- src/main/java/com/example/Project1/controller/admin/ProductController.java
+++ src/main/java/com/example/Project1/controller/admin/ProductController.java
@@ -2,15 +2,12 @@ package com.example.Project1.controller.admin;
 
 import com.example.Project1.domain.dto.request.product.ProductListView;
 import com.example.Project1.domain.dto.request.product.ProductRegRequest;
-import com.example.Project1.domain.entity.Category;
-import com.example.Project1.domain.entity.DeliveryType;
 import com.example.Project1.domain.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,6 +24,8 @@ public class ProductController {
     @GetMapping("/new")
     public String regForm(Model model) {
         model.addAttribute("product",new ProductRegRequest());
+        model.addAttribute("categories", service.getCategories());
+        model.addAttribute("deliveryTypes", service.getDeliveryTypes());
         return "/admin/products/reg";
     }
     @PostMapping("/new")
@@ -35,14 +34,8 @@ public class ProductController {
         log.info("ProductRegRequest = {}", productRegRequest);
         return "redirect:/admin/products";
     }
-    @ModelAttribute("categories")
-    public List<Category> categories() {
-        return service.getCategories();
-    }
-    @ModelAttribute("deliveryTypes")
-    public List<DeliveryType> deliveryCodes() {
-        return service.getDeliveryTypes();
-    }
+
+
     @GetMapping
     public String showList(Model model) {
         List<ProductListView> list = service.getList();
